5747872b361f4205e7923f8e8549a93c5895dbc7
Alex R <alexei.rudenko@gmail.com>
2015-07-04 17:46:39 +0200

    Corrected imports

--- tasks/js.js
+++ tasks/js.js
@@ -1,6 +1,4 @@
 var gulp = require('gulp');
-var uglify = require('gulp-uglify');
-var concat = require('gulp-concat');
 var jspm = require('jspm');
 
 gulp.task('js', function(done) {
--- tasks/styles.js
+++ tasks/styles.js
@@ -3,7 +3,6 @@ var vcl = require('gulp-vcl-preprocessor');
 var autoprefixer = require('gulp-autoprefixer');
 var concat = require('gulp-concat');
 var minifyCss = require('gulp-minify-css');
-var uncss = require('gulp-uncss');
 var glob = require('glob');
 var plumber = require('gulp-plumber');
 
@@ -14,14 +13,6 @@ gulp.task('styles', function() {
     .pipe(plumber())
     .pipe(vcl())
     .pipe(concat('main.css'));
-
-  if (site.uncss) {
-    pipe = pipe.pipe(uncss({
-      html: glob.sync('dist/**/*.html'),
-      ignore: ['#bounce-in', 'bounce-in', '.bounce-in', '.vclTimelineContent.bounce-in'],
-      media: ['(max-width: 640px)', '(min-width: 390px) and (max-width: 1280px)', '(max-width: 390px)', '(min-width: 1280px)']
-    }));
-  }
 
   return pipe.pipe(autoprefixer())
     .pipe(minifyCss({
