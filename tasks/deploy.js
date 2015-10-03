b81a3854d46b4854089821f7fa7322fc579a3210
Alex R <alexei.rudenko@gmail.com>
2015-10-03 16:05:58 +0200

    fix

--- tasks/deploy.js
+++ tasks/deploy.js
@@ -1,48 +1,33 @@
 var gulp = require('gulp');
-gulp.task('deploy', function() {
-  var ghPages = require('gulp-gh-pages');
-  var rsync = require('gulp-rsync');
-  var run = require('../lib/run');
+var site = global.site;
+var ghPages = require('gulp-gh-pages');
+var rsync = require('gulp-rsync');
+var run = require('../lib/run');
 
-  var site = global.site;
-
-  function doDeploy() {
-    switch (site.deploy.method) {
-      case 'github':
-        return gulp.src('./dist/**/*')
-          .pipe(ghPages({
-            remoteUrl: site.deploy.url,
-            branch: site.deploy.branch || 'master'
-          }));
-      case 'rsync':
-        return gulp.src(['dist/**/*'])
-          .pipe(rsync(site.deploy));
+function getBranch(data) {
+  var spl = data.split('\n');
+  var branchName = null;
+  spl.forEach(function(branch) {
+    if (branch[0] === '*') {
+      branchName = branch.substring(2).trim();
     }
-  }
+  });
+  return branchName;
+}
 
-  function getBranch(data) {
-    var spl = data.split('\n');
-    var branchName = null;
-    spl.forEach(function(branch) {
-      if (branch[0] === '*') {
-        branchName = branch.substring(2).trim();
-      }
-    });
-    return branchName;
-  }
+function upToDate(name) {
+  return run('git remote show origin', true).then(function(output) {
+    var re = new RegExp(name + ' \\(up to date\\)', 'gi');
+    if (re.test(output)) {
+      console.log('Current branch is up-to-date');
+      return true;
+    } else {
+      throw new Error('Current branch is not up-to-date');
+    }
+  });
+}
 
-  function upToDate(name) {
-    return run('git remote show origin', true).then(function(output) {
-      var re = new RegExp(name + ' \\(up to date\\)', 'gi');
-      if (re.test(output)) {
-        console.log('Current branch is up-to-date');
-        return true;
-      } else {
-        throw new Error('Current branch is not up-to-date');
-      }
-    });
-  }
-
+gulp.task('check-deploy', function(done) {
   run('git branch', true).then(function(data) {
     var currentBranch = getBranch(data);
     console.log('Current branch: ' + currentBranch);
@@ -50,12 +35,30 @@ gulp.task('deploy', function() {
       console.log('Cannot detect current git branch name');
     }
     upToDate(currentBranch).then(function() {
-      doDeploy();
+      done();
     }).catch(function(err) {
       console.error(err);
       console.error('Deployment aborted!');
+      done(err);
     });
   }).catch(function(err, data) {
     console.log(err, data);
+    done(err);
   });
+});
+
+gulp.task('deploy', ['check-deploy'], function() {
+  console.log('Doing deployment via ' + site.deploy.method);
+  switch (site.deploy.method) {
+    case 'github':
+      return gulp.src('./dist/**/*')
+        .pipe(ghPages({
+          remoteUrl: site.deploy.url,
+          branch: site.deploy.branch || 'master'
+        }));
+    case 'rsync':
+      return gulp.src('dist/**/*')
+        .pipe(rsync(site.deploy));
+  }
+  return doDeploy();
 });
